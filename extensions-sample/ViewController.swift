473a4c71283a2cab77dd09f7d71cb73eceba2778
Joca <joca@bwmobi.com.br>
2017-09-24 13:47:01 -0300

    Initial Commit

---
+++ extensions-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  extensions-sample
+//
+//  Created by João Carlos Brandão on 24/09/17.
+//  Copyright © 2017 BWmobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
